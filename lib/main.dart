30c65400ffaa0f4a7594be59e5ae869a9080d2ce
thanyarat65 <thanyaratw65@nu.ac.th>
2024-09-01 22:59:55 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/menus.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Simple App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Simple App"),
+      ),
+      body: Menus(),
+    ),
+  ));
+}
